8d573633db8074a3568ed9bcca750a345993effe
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-01-26 01:20:31 +0600

    skill add

--- src/Component/Skill.jsx
+++ src/Component/Skill.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import { FaFire } from "react-icons/fa";
+import js from '../assets/js.svg'
+import html from '../assets/html.svg'
+import css from '../assets/css.svg'
+import figma from '../assets/figma.svg'
+import talwind from '../assets/talwind.svg'
+import recet from '../assets/react.svg'
+import redux from '..//assets/redux.svg'
+import firebase from '../assets/firebase.svg'
+import express from '../assets/exprees.png'
+import node from '../assets/node.png'
+import api from '../assets/api.svg'
+import next from '../assets/next.svg'
+import mui from '../assets/mui.svg'
+import ri from '../assets/intrei.svg'
 const Skill = () => {
   return (
     <>
@@ -14,7 +28,55 @@ const Skill = () => {
 
       {/* content */}
     <div className='mt-10  flex justify-center'>
-     <div className='w-1/5'></div>
+     <div className='w-1/5   '>
+        <img src={html} className='rounded-full p-4   border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={css} className='rounded-full p-4   border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={js} className='rounded-full p-4  border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={figma} className='rounded-full p-4  border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={talwind} className='rounded-full p-4  border-8 border-comon w-[150px]' />
+     </div>
+    </div>
+  {/* second */}
+    <div className='mt-14  flex justify-center'>
+     <div className='w-1/5   '>
+        <img src={recet} className='rounded-full p-6   border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={redux} className='rounded-full p-6  border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={firebase} className='rounded-full p-4  border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={express} className='rounded-full p-4  border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={node} className='rounded-full p-6  border-8 border-comon w-[150px]' />
+     </div>
+    </div>
+ {/* thrid */}
+    <div className='mt-14  flex justify-center'>
+     <div className='w-1/5   '>
+        <img src={api} className='rounded-full p-4   border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={next} className='rounded-full p-4   border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={ri} className='rounded-full p-4   border-8 border-comon w-[150px]' />
+     </div>
+     <div className='w-1/5   '>
+        <img src={mui} className='rounded-full p-4   border-8 border-comon w-[150px]' />
+     </div>
+   
     </div>
 
 
